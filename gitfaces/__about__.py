2d26f0c58237e69f9c5382956434b95e889eef54
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-23 16:23:36 +0200

    version bump

--- gitfaces/__about__.py
+++ gitfaces/__about__.py
@@ -2,8 +2,8 @@
 #
 __author__ = 'Nico Schlömer'
 __email__ = 'nico.schloemer@gmail.com'
-__copyright__ = 'Copyright (c) 2017, %s <%s>' % (__author__, __email__)
+__copyright__ = 'Copyright (c) 2017, {} <{}>'.format(__author__, __email__)
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __maintainer__ = 'Nico Schlömer'
 __status__ = 'Development Status :: 4 - Beta'
