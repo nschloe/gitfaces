50db9adeabfd1589685bd5852efa347383676237
Nico Schlömer <nico.schloemer@gmail.com>
2020-09-15 11:21:07 +0200

    add setup.cfg

--- gitfaces/__init__.py
+++ gitfaces/__init__.py
@@ -1,20 +1,6 @@
-from gitfaces.__about__ import (
-    __author__,
-    __copyright__,
-    __email__,
-    __status__,
-    __version__,
-)
+from gitfaces.__about__ import __version__
 
 from . import cli
 from .main import fetch
 
-__all__ = [
-    "__author__",
-    "__email__",
-    "__copyright__",
-    "__version__",
-    "__status__",
-    "fetch",
-    "cli",
-]
+__all__ = ["__version__", "fetch", "cli"]
